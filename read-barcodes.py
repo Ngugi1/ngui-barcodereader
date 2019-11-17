9cee7dd2d1b4ad5e7cfafc476096cb6e0206320d
Ngugi <samuel.ngugi@vub.be>
2019-11-17 08:53:25 +0100

    Latest

--- read-barcodes.py
+++ read-barcodes.py
@@ -1,7 +1,7 @@
-import numpy as np
+# import the necessary packages
+from pyzbar import pyzbar
 import cv2
 import time
-from pyzbar import pyzbar
 import requests
 
 API_END_POINT = "http://127.0.0.1:3000/"
@@ -11,30 +11,47 @@ def sendBarcodes(barcodes):
     res = requests.post(API_END_POINT + "upload", json={"barcodes": data})
     print("Results are:: {}\n").format(res.text)
 
-cap = cv2.VideoCapture(1)
-time.sleep(3)
-focus = 0  # min: 0, max: 255, increment:5
-cap.set(28, focus)
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vc = cv2.VideoCapture(0)
+# vs = VideoStream(usePiCamera=True).start()
+time.sleep(1.0)
 found = set()
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # print("{}\n").format(ret)
+# loop over the frames from the video stream
+while True:
+    # grab the frame from the threaded video stream and resize it to
+    ret, frame = vc.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # decode barcodes found in the frame
+    cv2.imshow('frame',gray)
+    # Gray frames 
+    # find the barcodes in the frame and decode each of the barcodes
     barcodes = pyzbar.decode(frame)
-    # Loop throught all barcodes
+    # loop over the detected barcodes
     for barcode in barcodes:
+        # extract the bounding box location of the barcode and draw
+        # the bounding box surrounding the barcode on the image
+        (x, y, w, h) = barcode.rect
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # the barcode data is a bytes object so if we want to draw it
+        # on our output image we need to convert it to a string first
         barcodeData = barcode.data.decode("utf-8")
         barcodeType = barcode.type
-        print("{}, {}").format(barcodeData, barcodeType)
+        # draw the barcode data and barcode type on the image
+        text = "{} ({})".format(barcodeData, barcodeType)
+        cv2.putText(frame, text, (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        # if the barcode text is currently not in our CSV file, write
+        # the timestamp + barcode to disk and update the set
         if barcodeData not in found:
+            print("{}\n").format(barcodeData)
             found.add(barcodeData)
-            # send it to the server here
             # sendBarcodes(list(found))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-# When everything done, release the capture
-cap.release()
+        # show the output frame
+        cv2.imshow("Barcode Scanner", gray)
+# close the output CSV file do a bit of cleanup
+print("[INFO] cleaning up...")
 cv2.destroyAllWindows()
+vs.stop()
+
+# Receives a list of barcodes and sends them to the server
